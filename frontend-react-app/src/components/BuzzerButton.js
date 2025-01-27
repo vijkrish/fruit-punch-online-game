df2d610c00deabcbba5d36c52c71b2260a6a2bf9
drajamani <drajamani@paloaltonetworks.com>
2025-01-26 18:03:14 -0800

    add changes for buzzer press logic

--- frontend-react-app/src/components/BuzzerButton.js
+++ frontend-react-app/src/components/BuzzerButton.js
@@ -3,10 +3,24 @@
 
 import React from 'react';
 
-const BuzzerButton = () => {
-  const handleBuzzerClick = () => {
-    // Add logic to handle buzzer click event
-    console.log('Buzzer button clicked!');
+const BuzzerButton = ({ playerId }) => {
+  const handleBuzzerClick = async () => {
+    try {
+      const response = await fetch(`http://127.0.0.1:5000/hit-bell/${playerId}`, {
+        method: 'GET',
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        console.log('Bell hit:', data);
+        // Add any additional logic you want to perform on successful bell hit
+      } else {
+        console.error(data.error);
+      }
+    } catch (error) {
+      console.error('Error hitting bell:', error);
+    }
   };
 
   return (
@@ -19,6 +33,7 @@ const BuzzerButton = () => {
       border: 'none',
       borderRadius: '5px',
       cursor: 'pointer',
+      flex: '3',
     }}>
       <img src="images/buzzer.jpg" alt="Buzzer" style={{ width: '50px', height: '50px' }} />
     </button>
